ec74f84615f86b51a8a684430e171b785e2114fb
Bd999 <45157599+18510047382@users.noreply.github.com>
2019-09-03 19:59:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-document.querySelector('h2').onmouseover = function() {
-    document.querySelector('#card').style.opacity = '1';
-}
+(function(window, document) {
+    var isForceOpenCard = false;
 
-document.querySelector('h2').onmouseout = function() {
-    document.querySelector('#card').style.opacity = '0';
-}
+    document.querySelector('h2').onmouseover = function() {
+        document.querySelector('#card').style.opacity = '1';
+    }
+
+    document.querySelector('h2').onmouseout = function() {
+        if (!isForceOpenCard) {
+            document.querySelector('#card').style.opacity = '0';
+        }
+    }
+
+    document.querySelector('h2').onclick = function() {
+        setTimeout(function(){
+            document.querySelector('#card').style.opacity = '1';
+            isForceOpenCard = true;
+        }, 0)
+    }
+
+    window.onclick = function() {
+        document.querySelector('#card').style.opacity = '0';
+        isForceOpenCard = false;
+    }
+})(window, document);
